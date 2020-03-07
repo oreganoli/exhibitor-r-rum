2e05854d9e41fe635ae978a242ac4bf27eba2252
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-03-07 21:25:48 +0100

    display a page counter

--- app/src/main/java/xyz/oreganoli/exhibitorRerum/MainActivity.kt
+++ app/src/main/java/xyz/oreganoli/exhibitorRerum/MainActivity.kt
@@ -2,13 +2,24 @@ package xyz.oreganoli.exhibitorRerum
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import org.koin.core.KoinComponent
 import org.koin.core.inject
+import xyz.oreganoli.exhibitorRerum.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), KoinComponent {
+    private var page = 1
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey("CURRENT_POST_PAGE")) {
+                page = savedInstanceState.getInt("CURRENT_POST_PAGE")
+            }
+        }
         val postRepo: PostRepo by inject()
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.setVariable(BR.page, page.toString())
+        binding.executePendingBindings()
     }
 }
